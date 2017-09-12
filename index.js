bf7856d49e2c191383c578ec316fa4958badec14
himanshu <himanshuchawla2014@gmail.com>
2017-09-12 11:46:27 -0700

    port changes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express=require('express')
 var path = require('path')
 var app=express()
+port=process.env.PORT || 8000
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(2200,function(){
+app.listen(port,function(){
     console.log("i m listenning")
 })
