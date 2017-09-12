95d2de0a9cf9dd3454eb7988f2bee42370388550
himanshu <himanshuchawla2014@gmail.com>
2017-09-12 11:50:46 -0700

    route change

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var path = require('path')
 var app=express()
 port=process.env.PORT || 8000
 app.use(express.static(path.join(__dirname, 'public')));
+app.get("/",function(req,res){
+    res.render("index.html")
+})
 app.listen(port,function(){
     console.log("i m listenning")
 })
